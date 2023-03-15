ad8be79f7900bf06fdc6b083b1db9e8cedddea22
Emergency Food <102189725+KittyWitty2022@users.noreply.github.com>
2023-03-15 16:26:01 -0700

    Mongo saves object entry

--- mongo.py
+++ mongo.py
@@ -7,16 +7,17 @@ db = client.get_database('cats')
 records = db.CatBreeds
 records.count_documents({})
 print("Hi I'm here")
-# #connect(db="cats", host="localhost")
-# class CatBreed(Document):
-#     breed = StringField(required=True, max_length=64)
-#     style = StringField(max_length=255)
-#     number_of_subspecies = IntField()
+#NTS:put the uri like what's for client into the host parameter of connect for cloud db
+connect(db="cats", host="mongodb+srv://artkittywitty:Yh9nW31GlmTHWPmz@kittywittydb.0d0dir0.mongodb.net/?retryWrites=true&w=majority")
+class CatBreed(Document):
+    breed = StringField(required=True, max_length=64)
+    style = StringField(max_length=255)
+    number_of_subspecies = IntField()
 
-# b = CatBreed (
-#     breed = "Siamese",
-#     style="Fancy",
-#     number_of_subspecies = 5
-# )
+b = CatBreed (
+    breed = "Siamese",
+    style="Fancy",
+    number_of_subspecies = 5
+)
 
-# b.save()
+b.save()
